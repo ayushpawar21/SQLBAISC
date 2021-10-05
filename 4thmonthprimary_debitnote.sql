26a0865f630a9f2eee0bac004023383263b114f9
AAYUSH PAWAR <47273528+ayushpawar21@users.noreply.github.com>
2021-10-05 23:43:33 +0530

    remove

--- 4thmonthprimary_debitnote.sql
+++ 4thmonthprimary_debitnote.sql
@@ -1,3 +1,6 @@
 SELECT * FROM TempTradeSchemeClaimDetails  WHERE Schid NOT IN (SELECT Schid FROM TradeSchemeFactors )
-###################################################
+
+
+
+
 select * from SchemeMaster where SchId=''
