6728a511c655d4fedc589a8574cac6731f7809c6
AAYUSH PAWAR <47273528+ayushpawar21@users.noreply.github.com>
2021-10-27 16:04:07 +0530

    Update RetailerMaster.sql

--- RetailerMaster.sql
+++ RetailerMaster.sql
@@ -23,3 +23,19 @@ delete from ErrorLog
 exec proc_Cn2cs_retailermasterdownload_new 0
 select * from errorlog
 rollback tran
+
+-------------------------------------------------------------
+
+select * from CustomUpDownload where UpDownload='download' and Module like '%ret%'
+
+begin tran
+delete from ErrorLog
+exec Proc_CN2Cs_RetailerMasterDownload_New 0
+select * from ErrorLog
+rollback tran
+
+SELECT * FROM CN2Cs_Prk_RetailerMasterDownload WHERE (DATALENGTH(RtrAddress1) > 50 OR DATALENGTH(RtrAddress2) > 50 OR DATALENGTH(RtrAddress3) > 50)
+SELECT LEN(RtrName),* FROM CN2Cs_Prk_RetailerMasterDownload WHERE (DATALENGTH(RtrName) > 50)
+SELECT COUNT(*) FROM CN2Cs_Prk_RetailerMasterDownload
+SELECT * FROM Retailer
+
